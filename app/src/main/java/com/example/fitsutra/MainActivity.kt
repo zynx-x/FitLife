6ab57d1fab854bd7e002aa590b02f9a39def6470
Samarpita Das <samarpitabdas@gmail.com>
2024-09-10 21:53:34 +0530

    bottom navigation

---
+++ app/src/main/java/com/example/fitsutra/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fitsutra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.fitsutra.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.tracker -> replaceFragment(TrackerFragment())
+                R.id.exercise -> replaceFragment(ExerciseFragment())
+                R.id.setting -> replaceFragment(SettingFragment())
+                R.id.report -> replaceFragment(ReportFragment())
+                else ->{
+
+                }
+
+            }
+            true
+
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame_layout,fragment)
+        fragmentTransition.commit()
+    }
+}
